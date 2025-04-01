bd38bb438cdfe5823fccf3a9e61a730f17cee752
SHIVAM KUMAR SHAW <108209696+mrshivamshaw@users.noreply.github.com>
2025-04-01 17:38:32 +0530

    Update settings.py

--- todo_site/settings.py
+++ todo_site/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'deccj*h)yc@%3*d6d68^p6q#a=32%qq#@%ksvc-^2-j+-$cl^l'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
